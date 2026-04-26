ca64140e6c8d57e111bc0a2512c1c604aab1f004
cesioaraya <94352674+cesioaraya@users.noreply.github.com>
2026-04-26 20:35:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -428,7 +428,7 @@ st.markdown(
     """
     <div style="text-align: center;">
         <p>¿Te ha sido útil esta herramienta? Considera invitarme un café para apoyar el mantenimiento del proyecto:</p>
-        <a href='https://ko-fi.com/cesioaraya' target='_blank'>
+        <a href='https://ko-fi.com/cesioaraya1' target='_blank'>
             <img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' />
         </a>
     </div>
